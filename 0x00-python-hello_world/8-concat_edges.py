dbf80fb53fe3b1e82d7fd0ea8edf206984afa402
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-15 06:44:28 -0800

    Prints particular words in a sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,5 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
 language that combines remarkable power with very clear syntax"
 print(str)
-str = (mystring.split(" ")[5, 6, 12, 0])
-print(str)
+print(mystring.split(" ")[5, 6, 12, 0])
