b974d327db616246a335dec02583e1e21f29d7a3
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-17 11:24:39 -0800

    prints numbers from 1 to 100 with comma separation

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for n in range(1, 101):
         if (n % 3 == 0 and n % 5 == 0):
             print('FizzBuzz', end=' ')
-        elif (n & 3 == 0):
+        elif (n % 3 == 0):
             print('Fizz', end=' ')
         elif (n % 5 == 0):
             print('Buzz', end=' ')
