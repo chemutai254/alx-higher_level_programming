cf57af9be6e723f0c1e38e96c7cd10f14ded5e7e
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-17 11:38:22 -0800

    creates a copy of the string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    str1 = ""
+    str_one = ""
     for m in range(len(str)):
         if (m != n):
-            str1 += str[m]
-            return str1
+            str_one += str[m]
+            return str_one
