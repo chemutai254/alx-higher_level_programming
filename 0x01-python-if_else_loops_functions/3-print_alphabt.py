1c0aca5b6ebea2cdb89c1f2666d0c1b9b28eaba7
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-17 11:57:07 -0800

    prints ASCII except q and e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for n in range(97, 123):
     if (n != 101 and n != 113):
-        print('Prints ASCII except q and e {:c}'.format(n), end='')
+        print('{:c}'.format(n), end='')
