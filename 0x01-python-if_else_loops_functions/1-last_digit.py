3543ed8bb96463687232780d3944528c3ed904a6
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-17 09:47:46 -0800

    prints last digit in a random range

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,9 +6,9 @@ if number < 0:
 elif number >= 0:
     last_digit = number % 10
 if last_digit > 5:
-    n = 'Last digit of {:d} is {:d} and is greater than 5'
+    n = 'Last digit of {} is {} and is greater than 5'
 elif last_digit < 6 and last_digit != 0:
-    n = 'Last digit of {:d} is {:d} and is less than 6 and not 0'
+    n = 'Last digit of {} is {} and is less than 6 and not 0'
 elif last_digit == 0:
-    n = 'Last digit of {:d} is {:d} and is 0'
+    n = 'Last digit of {0} is {1} and is 0'
 print(n.format(number, last_digit))
