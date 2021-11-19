276e48603f939ad82abd514e218614c507fbacc0
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-19 03:07:41 -0800

    import functions that does some calculations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,11 @@ from calculator_1 import sub, add, mul, div
 
 
 if __name__ == "__main__":
+    """
+
+    Prints the results of subtraction, addition, multiplication and dividsion
+
+    """
         a = 10
         b = 5
         print('{:d} - {:d}'.format(a, b, sub(a, b)))
