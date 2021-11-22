98625bb7d7798b868bd7ce8292886f034fffbc96
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-22 03:07:58 -0800

    arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,3 +3,12 @@ import sys
 
 if __name__ == '__main__':
 
+    counter = len(sys.argv) - 1)
+    if (counter) == 0:
+        print('0 arguments.{}')
+    elif (counter) == 1:
+        print('1 argument: {}')
+    else:
+        print('{} arguments:'.format(counter))
+        for n in range(counter):
+            print('{}: {}'.format(n + 1, sys.argv[n + 1]))
