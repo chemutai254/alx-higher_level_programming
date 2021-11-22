717cb50eebed7f2d5c363909966eeec9cd070de1
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-22 10:29:10 -0800

    easy print

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+if __name__ == '__main__':
+    a = int(a)
+    b= int(b)
+
+
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,12 @@ import sys
 
 if __name__ == '__main__':
 
-    counter = len(sys.argv) - 1)
-    if (counter) == 0:
-        print('0 arguments.{}')
-    elif (counter) == 1:
-        print('1 argument: {}')
+    count = len(sys.argv) - 1)
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print('{} arguments:'.format(counter))
-        for n in range(counter):
-            print('{}: {}'.format(n + 1, sys.argv[n + 1]))
+        print("{} arguments:".format(count))
+    for n in range(count):
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
