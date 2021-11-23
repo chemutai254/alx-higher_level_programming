fbfaecf40074f25ca7a920c12fc1a6da7930e20c
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-22 23:05:29 -0800

    prints a list of integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
-def print_list_integer(my_list):
-    my_list = [1, 2, 3, 4, 5]
-    str = "The number is {}"
-    print(str.format(*my_list))
+def print_list_integer(my_list=[]):
+    print('{:d}'.format(*my_list))
