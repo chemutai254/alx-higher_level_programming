4e25a4a922915c7c965bba56a375844a2cfe04c1
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-23 23:22:35 -0800

    function that performs math

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import sub, add, mul, div
-
-
-if __name__ == "__main__":
-    """
-
-    Prints the results of subtraction, addition, multiplication and dividsion
-
-    """
-        a = 10
-        b = 5
-        print('{:d} - {:d}'.format(a, b, sub(a, b)))
-        print('{:d} + {:d}'.format(a, b, add(a, b)))
-        print('{:d} * {:d}'.format(a, b, mul(a, b)))
-        print('{:d} / {:d}'.format(a, b, div(a, b)))
+if __name__ == '__main__':
+    from calculator_1 import sub, add, mul, div
+    a = 10
+    b = 5
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
