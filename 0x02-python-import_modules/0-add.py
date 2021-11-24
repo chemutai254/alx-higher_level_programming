93702e2afad28a077005516e4f34f8c38572ac6a
chemutai254 <nancy.chemutai254@gmail.com>
2021-11-23 23:14:25 -0800

    imports a function ad prints the sum

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,9 @@ from add_0 import add
 
 
 if __name__ == "__main__":
-    def add(a, b):
+    main()
+
+    def main():
         a = 1
         b = 2
-        print('{:d} + {:d}'.format(a, b, add(a, b)))
+        print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
