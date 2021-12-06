afd8bdc206618d752866531061e6c9b50fd65b67
chemutai254 <nancy.chemutai254@gmail.com>
2021-12-06 04:13:04 -0800

    prints elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     length = 0
-    count = 0
-    for m in my_list:
-        length += 1
 
-        for n in range(x):
-            try:
-                print('{:d}'.format(my_list[n]), end="")
-            except:
-                print('')
-                return length
-            count += 1
-            print("")
-            return count
+    try:
+        for n in my_list:
+            if length < x:
+                print('{:d}'.format(my_list[length]), end='')
+                length += 1
+
+                print()
+    except:
+        pass
+    return length
