81d32713fb5ca9d8622b9b5826b12f7f906d24e4
chemutai254 <nancy.chemutai254@gmail.com>
2021-12-06 03:57:49 -0800

    prints integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    integer = 0
     try:
-        if value == integer:
-            print('{:d} True'.format(value))
-        else:
-            print('{:d} False'.format(value))
+        print('{:d}'.format(value))
+        return True
     except value:
-        print('the value is neither true or false')
+        return False
