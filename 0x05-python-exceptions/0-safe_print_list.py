f3de981b1cbd2859e1da56dc68d03f62f8637045
chemutai254 <nancy.chemutai254@gmail.com>
2021-12-06 04:15:05 -0800

    prints elements of a list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,11 @@ def safe_print_list(my_list=[], x=0):
     try:
         for n in my_list:
             if length < x:
-                print('{:d}'.format(my_list[length]), end='')
+                print('{}'.format(my_list[length]), end='')
                 length += 1
 
                 print()
-    except:
+    except TypeError:
         pass
-    return length
+    finally:
+        return length
