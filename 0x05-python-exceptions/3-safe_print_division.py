addd585529e555a16b74213075c8ce2686592ac9
chemutai254 <nancy.chemutai254@gmail.com>
2021-12-12 01:06:44 -0800

    divides two integers

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a / b
+        result = a/b
     except ZeroDivisionError:
         pass
     finally:
-        if result != 0:
-            print('Result equals: {}'.format(result))
+        if b != 0:
+            print("Inside result: {}".format(result))
             return result
         else:
-            print('Check the type of data')
-            return Null
+            print("Inside result: None")
+            return None
