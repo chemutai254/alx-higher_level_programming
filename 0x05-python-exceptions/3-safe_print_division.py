f4149b2d678c316a61281c2bc20e1816a48cc35a
chemutai254 <nancy.chemutai254@gmail.com>
2021-12-12 00:41:01 -0800

    division

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-	try:
-	    result = a / b
-	except ZeroDivisionError:
-		pass
-	finally:
-	    if result != 0:
-	        print('Result equals: {}'.format(result))
-	        return result
-	    else:
-		print('Check the type of data')
-	        return Null
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        if result != 0:
+            print('Result equals: {}'.format(result))
+            return result
+        else:
+            print('Check the type of data')
+            return Null
