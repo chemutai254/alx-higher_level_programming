8755e072d80171009785c41eea1f28c31cc2f39a
chemutai254 <nancy.chemutai254@gmail.com>
2021-12-14 23:54:02 -0800

    creates a new class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,10 +10,12 @@ class Rectangle(object):
         self.width = width
         self.height = height
 
+        @property
         def width(self):
             """property for attribute width"""
             return self.__width
 
+        @width.setter
         def width(self, value):
             """setting values to width"""
             if not isinstance(value, int):
@@ -22,10 +24,12 @@ class Rectangle(object):
                 raise ValueError('width must be >= 0')
             self.__width = value
 
+            @property
             def height(self):
                 """property for attribute height"""
                 return self.__height
 
+            @height.setter
             def height(self, value):
                 """setting values to width"""
                 if not isinstance(value, int):
