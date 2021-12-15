431c624650c12cbf2a31dbfa9bfe8e6e580891f3
chemutai254 <nancy.chemutai254@gmail.com>
2021-12-14 23:23:39 -0800

    creates an empty class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-""" File name : 0-rectangle.py
-    Simple rectangle : an empty class Rectangle that defines a rectangle
-    It is not allowed to import any module
-"""
-
-
 class Rectangle(object):
     pass
