9879330674ee1ebf650bbbba53e917363accbab7
chemutai254 <nancy.chemutai254@gmail.com>
2022-02-28 05:23:41 -0800

    prints scrip messages using arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 let str;
-if (process.argv.length < 1)
+if (process.argv.length < 3)
 	str = 'No argument';
-else if (process.argv.length === 1)
+else if (process.argv.length === 3)
 	str = 'Argument found';
 else
 	str = 'Arguments found';
