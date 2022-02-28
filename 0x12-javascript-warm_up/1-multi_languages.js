f67ca4defebb3ebd4a187e2b546945d123c61130
chemutai254 <nancy.chemutai254@gmail.com>
2022-02-28 05:01:15 -0800

    prints script in three lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-let languages = "C is fun" + <br> + "Python is cool" + <br> + "JavaScript is amazing";
-console.log(languages);
+let first_line = 'C is fun';
+let second_line = 'Python is cool';
+let third_line = 'JavaScript is amazing';
+console.log(first_line);
+console.log(second_line);
+console.log(third_line);
