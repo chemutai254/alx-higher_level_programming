880a5e267ae321e3abc86290f8f2edcad3e471ea
chemutai254 <nancy.chemutai254@gmail.com>
2022-02-28 07:27:10 -0800

    script that prints 3 lines using an array of string

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const loop_languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (i < loop_languages.length) {
+  console.log(loop_languages[i]);
+  i++;
+}
