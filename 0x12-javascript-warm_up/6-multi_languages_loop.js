da259804d72422cfbc3cbf740b1c1149031e1134
chemutai254 <nancy.chemutai254@gmail.com>
2022-02-28 07:30:27 -0800

    script that prints 3 lines using an array of string

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const loop_languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const loop = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 let i = 0;
-while (i < loop_languages.length) {
-  console.log(loop_languages[i]);
+while (i < loop.length) {
+  console.log(loop[i]);
   i++;
 }
