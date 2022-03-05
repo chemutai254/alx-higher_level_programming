e90de0040fe257811772004e35b2d56a3a8828e4
chemutai254 <nancy.chemutai254@gmail.com>
2022-03-04 21:34:25 -0800

    gets all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,5 +7,5 @@ import MySQLdb
 if __name__ = "__main__":
     conn = MySQLdb.connect(user=sys.argv[1], passd=sys.argv[2], db=sys.argv[3])
     i = conn.cursor()
-    i.execute("select * from 'states'")
+    i.execute("select * from 'states' ORDER BY 'id'")
     [print(state) for state in i.fetchall()]
