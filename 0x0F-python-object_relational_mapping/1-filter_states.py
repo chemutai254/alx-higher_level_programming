8af329e769f88b3f9eb67240851294ab8a39d522
chemutai254 <nancy.chemutai254@gmail.com>
2022-03-04 22:12:50 -0800

    Added an sql file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 """Lists all states with a name starting with N from hbtn_0e_0_usa database"""
-import 
+import sys
+import MySQLdb
+
+
+if __name__ = "__main__":
+    conn = MySQLdb(user=sys.argv[1], passd=sys.argv[2], db=sys.argv[3])
+    i = conn.cursor()
+    i.execute("SELECT * FROM 'states' ORDER BY 'id'")
+    [print(state) for state in i.fetchall() if state[1][0] == "N"]
