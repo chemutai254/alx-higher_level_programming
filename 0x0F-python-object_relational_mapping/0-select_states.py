e0a85c24fcf98e23bd80cf27e9eff1a9e154fc58
chemutai254 <nancy.chemutai254@gmail.com>
2022-03-04 21:15:02 -0800

    gets all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python3
 """Lists all states fro the database hbtn_0e_0_usa"""
-import MySQLdb
 import sys
+import MySQLdb
 
 
 if __name__ = "__main__":
-
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-
-    # begin cursor
-    curs = conn.cursor()
-
-    # query
-    curs.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = curs.fetchall()
-
-    # print query
-    for row in query_rows:
-        print(row)
-
-    # end cursor
-    curs.close()
-    conn.close()
+    conn = MySQLdb.connect(user=sys.argv[1], passd=sys.argv[2], db=sys.argv[3])
+    i = conn.cursor()
+    i.execute("select * from 'states'")
+    [print(state) for state in i.fetchall()]
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N from hbtn_0e_0_usa database"""
+import 
