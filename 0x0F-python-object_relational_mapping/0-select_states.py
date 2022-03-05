2de8a5d3c42c8c0f30722c320e2fcb2a5f495b08
chemutai254 <nancy.chemutai254@gmail.com>
2022-03-04 21:03:38 -0800

    gets all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,9 @@ import sys
 
 if __name__ = "__main__":
 
-    conn = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3],
-                        charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
     # begin cursor
     curs = conn.cursor()
 
