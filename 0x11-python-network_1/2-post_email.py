6d4b94d437bb57a7607a78c6f486139084089ff1
chemutai254 <nancy.chemutai254@gmail.com>
2022-03-18 05:16:19 -0700

    Python script that takes in URL and email and sends POST request

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Python script that takes in URL and email and sends POST"""
+import sys
+import urllib.request
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
