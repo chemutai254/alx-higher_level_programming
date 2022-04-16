92bf5fe48ccf8cbc930b7d4f74149b32a674cfa0
chemutai254 <nancy.chemutai254@gmail.com>
2022-04-16 05:00:58 -0700

    Script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
+  console.log(err || data);
 });
