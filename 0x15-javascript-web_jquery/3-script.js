be5f668d8c2f5665e2a369d4a3b6db6026a96dc7
chemutai254 <nancy.chemutai254@gmail.com>
2022-04-20 12:30:42 -0700

    adds a class red to the header

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,24 @@
+<html lang="en">
+  <head>
+    <title>Holberton School</title>
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <style>
+      .red {
+        color: #FF0000;
+      }
+    </style>
+  </head>
+  <body>
+    <header class="red"> 
+      First HTML page
+    </header>
+    <div id="red_header">Red header</div>
+    <footer>
+      Holberton School - 2022
+    </footer>
+    <script type="text/javascript" src="3-script.js">
+      $("#red_header").click(function() {
+      });
+    </script>
+  </body>
+</html>
