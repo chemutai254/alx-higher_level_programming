11c341f6a301de8d467610e6fd77182a86d539ca
chemutai254 <nancy.chemutai254@gmail.com>
2022-04-20 10:50:38 -0700

    Javascript script that updates the text color of the header element to red

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,18 @@
+<html lang='en'>
+  <head>
+    <title>Holberton School</title>
+    <script src='https://code.jquery.com/jquery-3.2.1.min.js' />
+  </head>
+  <body>
+    <header class='my_header'>
+      First HTML page
+    </header>
+    <footer>
+      Holberton School - 2022
+    </footer>
+    <script type='text/javascript' src='0-script.js'>
+      const element = document.querySelector(".my_header");
+      element.style.color = "red";
+    </script>
+  </body>
+</html>;
