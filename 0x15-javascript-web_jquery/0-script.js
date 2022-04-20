fbb57e13a18ee5be19f4a808b9381a519073aba7
chemutai254 <nancy.chemutai254@gmail.com>
2022-04-20 12:39:36 -0700

    Script that toggles the class of the header element when the user clicks on the tag

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,18 +1,1 @@
-<html lang='en'>
-  <head>
-    <title>Holberton School</title>
-    <script src='https://code.jquery.com/jquery-3.2.1.min.js' />
-  </head>
-  <body>
-    <header class='my_header'>
-      First HTML page
-    </header>
-    <footer>
-      Holberton School - 2022
-    </footer>
-    <script type='text/javascript' src='0-script.js'>
-      const element = document.querySelector(".my_header");
-      element.style.color = "red";
-    </script>
-  </body>
-</html>;
+document.querySelector('HEADER').style.color = '#FF0000';
--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,20 +1,3 @@
-<html lang='en'>
-  <head>
-    <title>Holberton School</title>
-    <script src='https://code.jquery.com/jquery-3.2.1.min.js' />
-  </head>
-  <body>
-    <header>
-      <div class='red_header'>
-        <h1>First HTML page</h1>
-      </div>
-    </header>
-    <div id='red_header'>Red header</div>
-    <footer>
-      Holberton School - 2017
-    </footer>
-    <script type='text/javascript' src='2-script.js'>
-      $("#red_header").click(function() <span style='color:red' />);
-    </script>
-  </body>
-</html>;
+$('DIV#red_header').click(function () {
+  $('HEADER').css('color', '#FF0000');
+});
