c9e1a5cc2a04f139707cddde1aaca8d215f3f680
chemutai254 <nancy.chemutai254@gmail.com>
2022-04-20 11:14:00 -0700

    Script with JQuery API

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,15 @@
+<html lang='en'>
+  <head>
+    <title>Holberton School</title>
+    <script src='https://code.jquery.com/jquery-3.2.1.min.js' />
+  </head>
+  <body>
+    <header>
+      .html('<span style='color:red'>First HTML page</span>')
+    </header>
+    <footer>
+      Holberton School - 2022
+    </footer>
+    <script type='text/javascript' src='1-script.js' />
+  </body>
+</html>;
